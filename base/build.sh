cdcb4b5d059494a4a52a65b608698f6c2d66dc8a
Kevin Boone <kevin@railwayterrace.com>
2017-05-25 07:29:55 +0100

    Tenatively modified base build installer to invoke android-elf-cleaner

--- base/build.sh
+++ base/build.sh
@@ -19,6 +19,11 @@ if [[ $? -ne 0 ]] ; then
     echo libfakechroot build failed ... stopping
     exit 1
 fi
+(cd unzip60; ./build.sh)
+if [[ $? -ne 0 ]] ; then
+    echo unzip60 build failed ... stopping
+    exit 1
+fi
 
 mkdir -p $BUILD_DIR
 
@@ -27,12 +32,20 @@ ZIP=/tmp/kbox4-base-installer.zip
 TARGET=$DIST/kbox4-install-base-$DEB_ARCH
 
 cp installer/* $BUILD_DIR
+
 cp $STAGING/kbox_shell-0.0.1/kbox_shell $BUILD_DIR/
+android-elf-cleaner $BUILD_DIR/kbox_shell
+
 cp $STAGING/fakechroot-2.16/libfakechroot.so $BUILD_DIR/
+android-elf-cleaner $BUILD_DIR/libfakechroot.so
+
 cp $STAGING/busybox-1.26.2/busybox $BUILD_DIR/
+android-elf-cleaner $BUILD_DIR/busybox
 
 (cd $STAGING; echo "\$AUTORUN\$>./kbox4-base-installer/install.sh" | zip -z -r $ZIP kbox4-base-installer)
 cat $STAGING/unzip60/unzipsfx $ZIP > $TARGET 
+
+android-elf-cleaner $TARGET
 
 rm $ZIP
 
